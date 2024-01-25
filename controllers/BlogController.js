b0ad8752acdcd559eb6678d54037c023395ee03e
minnica <minnica20@gmail.com>
2024-01-25 00:38:40 -0600

    add methods in controller

---
+++ controllers/BlogController.js
@@ -0,0 +1,66 @@
+// importamos el modelo 
+import BlogModel from "../models/BlogModel";
+
+// Métodos para el CRUD
+
+// Mostrar todos los registros
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await BlogModel.findAll()
+    res.json(blogs)
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+}
+
+// Mostrar un registro
+export const getBlog = async (req, res) => {
+  try {
+    const blog = BlogModel.findAll({
+      where: { id: req.param.id }
+    })
+    res.json(blog)
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+}
+
+// Crear un registro
+export const createBlog = async (req, res) => {
+  try {
+    await BlogModel.create(req.body)
+    res.json({
+      "message": "Registro creado correctamente"
+    })
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+}
+
+// Actualizar un registro
+export const updateBlog = async (req, res) => {
+  try {
+    await BlogModel.update(req.body, {
+      where: { id: req.param.id }
+    })
+    res.json({
+      "message": "Registro actualizado correctamente"
+    })
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+}
+
+// Eliminar un registro
+export const deleteBlog = async (req, res) => {
+  try {
+    BlogModel.destroy({
+      where: { id: req.param.id }
+    })
+    res.json({
+      "message": "Registro eleminado correctamente"
+    })
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+}
